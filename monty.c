64e4a993d9ac561f785b4f293369066c3232f33d
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-05-24 04:41:10 -0700

    test run

---
+++ monty.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+int main(int argc, char * argv[])
+{
+    int i = 1;
+    FILE *file_to_be_read;
+    char *path = argv[1];
+    char *current_line = NULL;
+    size_t current_line_size = 0;
+    ssize_t read;
+
+    if (argc > 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    file_to_be_read = fopen(path, "r");
+
+    if (file_to_be_read == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", path);
+        exit(EXIT_FAILURE);
+    }
+
+    while((read = getline(&current_line, &current_line_size, file_to_be_read)) != -1)
+    {
+        _line_tokenizer(current_line);
+    }
+    printf("\n");
+    free(current_line);
+    fclose(file_to_be_read);
+
+    return(0);
+}
---
+++ tokenizer.c
@@ -0,0 +1,37 @@
+#include "monty.h"
+
+int _line_tokenizer(char * current_line)
+{
+    char *token;
+    char *cmd;
+    char *cmd_val;
+    int actual_val;
+    int counter = 0;
+
+    token = strtok(current_line, " ");
+
+    while(token != NULL)
+    {
+        if (counter == 0)
+        {
+            cmd = token;
+        }
+        else if (counter == 1)
+        {
+            cmd_val = token;
+        }
+        else
+        {
+            return (1);
+        }
+
+        counter++;
+        token = strtok(NULL, " ");
+    }
+
+    actual_val = atoi(cmd_val);
+    printf("%s\n", cmd);
+    printf("%d\n", actual_val);
+
+    return (0);
+}
