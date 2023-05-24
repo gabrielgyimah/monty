bd6452933b8018734c87226f5b5bd58ac6b6bc03
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-05-24 05:38:39 -0700

    test run

--- tokenizer.c
+++ tokenizer.c
@@ -1,37 +1,43 @@
 #include "monty.h"
 
-int _line_tokenizer(char * current_line)
+/**
+ * line_tokenizer - splits the current line into toke
+ *
+ * @current_line: the string being split
+ *
+ * Return: success
+ */
+
+int line_tokenizer(char *current_line)
 {
-    char *token;
-    char *cmd;
-    char *cmd_val;
-    int actual_val;
-    int counter = 0;
+	char *token;
+	char *cmd;
+	char *cmd_val;
+	int actual_val;
+	int counter = 0;
 
-    token = strtok(current_line, " ");
+	token = strtok(current_line, " ");
 
-    while(token != NULL)
-    {
-        if (counter == 0)
-        {
-            cmd = token;
-        }
-        else if (counter == 1)
-        {
-            cmd_val = token;
-        }
-        else
-        {
-            return (1);
-        }
+	while (token != NULL)
+	{
+		if (counter == 0)
+		{
+			cmd = token;
+		}
+		else if (counter == 1)
+		{
+			cmd_val = token;
+		}
+		els
+		{
+			return (1);
+		}
+		counter++;
+		token = strtok(NULL, " ");
+	}
+	actual_val = atoi(cmd_val);
+	printf("Actual command: %s\n", cmd);
+	printf("command value: %d\n", actual_val);
 
-        counter++;
-        token = strtok(NULL, " ");
-    }
-
-    actual_val = atoi(cmd_val);
-    printf("%s\n", cmd);
-    printf("%d\n", actual_val);
-
-    return (0);
+	return (0);
 }
